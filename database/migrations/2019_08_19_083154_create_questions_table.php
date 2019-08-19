3909831598b17267f1a2b15cff976007b57cf608
Yannis <yannisdemacedo@gmail.com>
2019-08-19 11:46:52 +0200

    First Commit

---
+++ database/migrations/2019_08_19_083154_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question', 255);
+            $table->unsignedInteger('question_type_id');
+            $table->string('available_answer', 255)->nullable();
+            $table->unsignedInteger('sondage_id');
+            $table->timestamps();
+
+            $table->foreign('question_type_id')->references('id')->on('questions_type')->onDelete('CASCADE');
+            $table->foreign('sondage_id')->references('id')->on('sondages')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
---
+++ database/migrations/2019_08_19_083209_create_responses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('question_id');
+            $table->string('response', 255);
+            $table->string('available_response', 255)->nullable();
+            $table->string('hash_path', 255);
+            $table->timestamps();
+            
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+}
